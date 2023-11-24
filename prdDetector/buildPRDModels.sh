f7e138424afab91429f7caa60ed8c60c659e1d7b
prod3-lfproject <125928375+prod3-lfproject@users.noreply.github.com>
2023-11-24 10:49:15 -0500

    handle PD3PREFIX variable
    If PD3_PREFIX variable is set then use it as the root directory.

--- prdDetector/buildPRDModels.sh
+++ prdDetector/buildPRDModels.sh
@@ -1,31 +1,12 @@
-#/* SPDX-License-Identifier: Apache-2.0 */
-#/* Copyright (c) 2012-2023 Applied Communication Sciences
-# * (now Peraton Labs Inc.)
-# *
-# * This software was developed in work supported by the following U.S.
-# * Government contracts:
-# *
-# * HR0011-20-C-0160 HR0011-16-C-0061
-# * 
-# *
-# * Any opinions, findings and conclusions or recommendations expressed in
-# * this material are those of the author(s) and do not necessarily reflect
-# * the views, either expressed or implied, of the U.S. Government.
-# *
-# * DoD Distribution Statement A
-# * Approved for Public Release, Distribution Unlimited
-# *
-# * DISTAR Case 38846, cleared November 1, 2023
-# *
-# * Licensed under the Apache License, Version 2.0 (the "License");
-# * you may not use this file except in compliance with the License.
-# * You may obtain a copy of the License at
-# *
-# * http://www.apache.org/licenses/LICENSE-2.0
-# */
 #!/bin/bash
-PRDMODELS=`./getPrdParameter STOREDMODELDIR`
-MODELDIR=`./getPrdParameter MODELDIR`
+if test "$PD3_PREFIX" == ""
+then
+	echo "PD3_PREFIX not set, using parent dir"
+else
+	PD3_PREFIX="$PD3_PREFIX/"
+fi
+PRDMODELS=$PD3_PREFIX`./getPrdParameter STOREDMODELDIR`
+MODELDIR=$PD3_PREFIX`./getPrdParameter MODELDIR`
 TMPWORKINGDIR=`./getPrdParameter TMPWORKINGDIR`
 function execCmd {
         eval $1
